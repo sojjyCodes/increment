3f36466bcab6b5d4b12af821e25f02d0e9e8f8e4
sojjyCodes <ebenezeralagbe26@gmail.com>
2020-09-10 17:58:41 +0100

    Update

--- src/App.js
+++ src/App.js
@@ -1,23 +1,44 @@
-import React from "react";
+import React, { Component } from "react";
 import { Container, Navbar, Button } from "react-bootstrap";
 import "./App.css";
 
-function App() {
+export function Increase() {
+    alert("Increased")
+}
+
+export function Decrease() {
+    const count = 0;
+    alert("Decreased");
+}
+function App(props) {
+    const count = 0;
     return (
         <div className="App">
             <Container>
-                <Navbar expand="lg" variant="dark" bg="info">
+                <Navbar expand="lg" variant="dark" bg="info" style={styles.nav}>
                     <Navbar.Brand href="#" style={styles.title}>
-                        Increment
+                        Number Increment
                     </Navbar.Brand>
                 </Navbar>
             </Container>
+            <div className="count">
+                <center>{count}</center>
+            </div>
             <Button
-                variant="info"
+                onClick={() => Increase()}
+                variant="success"
                 style={styles.button}
                 className="button__text"
             >
                 Increase
+            </Button>
+            <Button
+                onClick={() => Decrease()}
+                variant="danger"
+                style={styles.button_2}
+                className="button__text"
+            >
+                Decrease
             </Button>
         </div>
     );
@@ -25,14 +46,23 @@ function App() {
 
 const styles = {
     title: {
+        fontSize: "35px",
         marginLeft: "500px",
     },
     button: {
-        fontSize: "40px",
+        fontSize: "30px",
         height: "60px",
         width: "150px",
-        marginTop: "440px",
-        marginLeft: "750px",
+        marginTop: "300px",
+        marginLeft: "690px",
+    },
+    button_2: {
+        position: "fixed",
+        fontSize: "30px",
+        height: "60px",
+        width: "150px",
+        marginLeft: "10px",
+        marginTop: "300px",
     },
 };
 export default App;
